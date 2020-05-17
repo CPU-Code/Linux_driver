ff05a058d77d00e8b53605e6d8b4ca57f85887f2
CPU_Code <923992029@qq.com>
2020-05-17 13:42:58 +0800

    printk注释

--- include/include/linux/kern_levels.h
+++ include/include/linux/kern_levels.h
@@ -5,14 +5,22 @@
 #define KERN_SOH	"\001"		/* ASCII Start Of Header */
 #define KERN_SOH_ASCII	'\001'
 
-#define KERN_EMERG	KERN_SOH "0"	/* system is unusable */
-#define KERN_ALERT	KERN_SOH "1"	/* action must be taken immediately */
-#define KERN_CRIT	KERN_SOH "2"	/* critical conditions */
-#define KERN_ERR	KERN_SOH "3"	/* error conditions */
-#define KERN_WARNING	KERN_SOH "4"	/* warning conditions */
-#define KERN_NOTICE	KERN_SOH "5"	/* normal but significant condition */
-#define KERN_INFO	KERN_SOH "6"	/* informational */
-#define KERN_DEBUG	KERN_SOH "7"	/* debug-level messages */
+/* printk 可以根据日志级别对消息进行分类 */
+/*  0 的优先级最高， 7 的优先级最低 */
+/* 
+ * 要设置消息级别，参考如下示例：
+ * printk(KERN_EMERG "gsmi: Log Shutdown Reason\n");
+ */
+/* printk 将 会 采 用 默 认 级 别 MESSAGE_LOGLEVEL_DEFAULT */
+/*  include/linux/printk.h  */
+#define KERN_EMERG	KERN_SOH "0"	/* 紧急事件，一般是内核崩溃 */
+#define KERN_ALERT	KERN_SOH "1"	/* 必须立即采取行动 */
+#define KERN_CRIT	KERN_SOH "2"	/*  临界条件，比如严重的软件或硬件错误 */
+#define KERN_ERR	KERN_SOH "3"	/* 错误状态，一般设备驱动程序中使用KERN_ERR 报告硬件错误 */
+#define KERN_WARNING	KERN_SOH "4"	/* 警告信息，不会对系统造成严重影响 */
+#define KERN_NOTICE	KERN_SOH "5"	/* 有必要进行提示的一些信息 */
+#define KERN_INFO	KERN_SOH "6"	/* 提示性的信息 */
+#define KERN_DEBUG	KERN_SOH "7"	/* 调试信息 */
 
 #define KERN_DEFAULT	""		/* the default kernel loglevel */
 
