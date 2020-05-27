78d7ef8ea41f664146795373b6c4bc40c535ba6a
CPU_Code <923992029@qq.com>
2020-05-27 21:36:18 +0800

    中断实验的驱动代码

--- code/14irq/irq.c
+++ code/14irq/irq.c
@@ -1,7 +1,7 @@
 /*
  * @Author: cpu_code
  * @Date: 2020-05-27 14:12:32
- * @LastEditTime: 2020-05-27 14:38:10
+ * @LastEditTime: 2020-05-27 20:06:42
  * @FilePath: \Linux_driver\code\14irq\irq.c
  * @Gitee: https://gitee.com/cpu_code
  * @CSDN: https://blog.csdn.net/qq_44226094
@@ -28,8 +28,8 @@
 #include <asm/uaccess.h>
 #include <asm/io.h>
 
-#define IMX6UIRQ_CNT		1			/* 设备号个数 	*/
-#define IMX6UIRQ_NAME		"irq"	    /* 名字 		*/
+#define IRQ_CNT		        1			/* 设备号个数 	*/
+#define IRQ_NAME		    "irq"	    /* 名字 		*/
 #define KEY0VALUE			0X01		/* KEY0按键值 	*/
 #define INVAKEY				0XFF		/* 无效的按键值 */
 #define KEY_NUM				1			/* 按键数量 	*/
@@ -44,24 +44,24 @@ struct irq_keydesc
     irqreturn_t (*handler)(int, void *);	/* 中断服务函数 */
 };
 
-/* imx6uirq设备结构体 */
+/* irq设备结构体 */
 struct irq_dev
 {
-    dev_t devid;			/* 设备号 	 */
-    struct cdev cdev;		/* cdev 	*/
-    struct class *class;	/* 类 		*/
-    struct device *device;	/* 设备 	 */
-    int major;				/* 主设备号	  */
-    int minor;				/* 次设备号   */
-    struct device_node	*nd; /* 设备节点 */
-    atomic_t keyvalue;		/* 有效的按键键值 */
-    atomic_t releasekey;	/* 标记是否完成一次完成的按键，包括按下和释放 */
-    struct timer_list timer;/* 定义一个定时器*/
+    dev_t               devid;			/* 设备号 	 */
+    struct cdev         cdev;		    /* cdev 	*/
+    struct class        *class;	        /* 类 		*/
+    struct device       *device;	    /* 设备 	 */
+    int                 major;			/* 主设备号	  */
+    int                 minor;			/* 次设备号   */
+    struct device_node	*nd;            /* 设备节点     */
+    atomic_t           keyvalue;		/* 有效的按键键值 */
+    atomic_t           releasekey;	    /* 标记是否完成一次完成的按键，包括按下和释放 */
+    struct timer_list  timer;           /* 定义一个定时器   */
     struct irq_keydesc irqkeydesc[KEY_NUM];	/* 按键描述数组 */
-    unsigned char curkeynum;				/* 当前的按键号 */
+    unsigned char      curkeynum;			/* 当前的按键号 */
 };
 
-struct irq_dev irq;	/* irq设备 */
+struct irq_dev irq; 	/* irq设备 */
 
 /**
  * @function: 中断服务函数，开启定时器，延时10ms，定时器用于按键消抖
@@ -135,7 +135,7 @@ static int keyio_init(void)
 	int ret = 0;
 
     irq.nd = of_find_node_by_path("/key");
-    if(imx6uirq.nd == NULL)
+    if(irq.nd == NULL)
     {
         printk("key node not find!\r\n");
 		return -EINVAL;
@@ -146,7 +146,7 @@ static int keyio_init(void)
     {
         irq.irqkeydesc[i].gpio = of_get_named_gpio(irq.nd ,"key-gpio", i);
 
-        if(imx6uirq.irqkeydesc[i].gpio < 0)
+        if(irq.irqkeydesc[i].gpio < 0)
         {
             printk("can't get key%d\r\n", i);
         }
@@ -155,37 +155,109 @@ static int keyio_init(void)
     /* 初始化key所使用的IO，并且设置成中断模式 */
 	for(i = 0; i < KEY_NUM; i++)
     {
+        memset(irq.irqkeydesc[i].name, 0, sizeof(name));	/* 缓冲区清零 */
+        sprintf(irq.irqkeydesc[i].name, "KEY%d", i);		/* 组合名字 */
 
+        gpio_request(irq.irqkeydesc[i].gpio, name);
+        gpio_direction_input(irq.irqkeydesc[i].gpio);
+
+        irq.irqkeydesc[i].irqnum = irq_of_parse_and_map(irq.nd, i);
+#if 0
+        irq.irqkeydesc[i].irqnum = gpio_to_irq(irq.irqkeydesc[i].gpio);
+#endif
+        printk("key%d:gpio = %d, irqnum = %d\r\n", i, 
+                                                   irq.irqkeydesc[i].gpio, 
+                                                   irq.irqkeydesc[i].irqnum);
+    }
+    /* 申请中断 */
+    irq.irqkeydesc[0].handler = key0_handler;
+    irq.irqkeydesc[0].value = KEY0VALUE;
+
+    for(i = 0; i < KEY_NUM; i++)
+    {
+        ret = request_irq(irq.irqkeydesc[i].irqnum, 
+                          irq.irqkeydesc[i].handler, 
+		                  IRQF_TRIGGER_FALLING | IRQF_TRIGGER_RISING, 
+                          irq.irqkeydesc[i].name, 
+                          &irq);
+        if(ret < 0)
+        {
+            printk("irq %d request failed!\r\n", irq.irqkeydesc[i].irqnum);
+            return -EFAULT;
+        }
     }
 
+    /* 创建定时器 */
+    init_timer(&irq.timer);
+    irq.timer.function = timer_function;
 
     return 0;
 }
 
 /**
- * @function: 
+ * @function: 打开设备
  * @parameter: 
+ *      inode 	: 传递给驱动的inode
+ *      filp 	: 设备文件，file结构体有个叫做private_data的成员变量
+ *                  一般在open的时候将private_data指向设备结构体
  * @return: 
- *     success: 
- *     error:
+ *     success: 0
+ *     error: 其他
  * @note: 
  */
 static int irq_open(struct inode *inode, struct file *filp)
 {
-    
+    filp->private_data = &irq;	/* 设置私有数据 */
+
+    return 0;
 }
 
 /**
- * @function: 
+ * @function: 从设备读取数据
  * @parameter: 
+ *      filp    : 要打开的设备文件(文件描述符)
+ *      buf     : 返回给用户空间的数据缓冲区
+ *      cnt     : 要读取的数据长度
+ *      offt    : 相对于文件首地址的偏移
  * @return: 
- *     success: 
- *     error:
+ *     success: 读取的字节数
+ *     error: 负值
  * @note: 
  */
 static ssize_t irq_read(struct file *filp, char __user *buf, size_t cnt, loff_t *offt)
 {
+    int ret = 0;
+	unsigned char keyvalue = 0;
+	unsigned char releasekey = 0;
+
+    struct irq_dev *dev = (struct irq_dev *)filp->private_data;
+
+    keyvalue = atomic_read(&dev->keyvalue);
+    releasekey = atomic_read(&dev->releasekey);
+
+    if(releasekey)
+    {
+        /* 有按键按下 */
+        if(keyvalue & 0x80) 
+        {
+            keyvalue &= ~0x80;
+            ret = copy_to_user(buf, &keyvalue, sizeof(keyvalue));
+        }
+        else
+        {
+            goto data_error;
+        }
+        atomic_set(&dev->releasekey, 0);/* 按下标志清零 */
+    }
+    else
+    {
+        goto data_error;
+    }
+
+    return 0;
     
+data_error:
+	return -EINVAL;
 }
 
 
@@ -206,7 +278,44 @@ static struct file_operations irq_fops = {
  */
 static int __init irq_init(void)
 {
+    /* 1、构建设备号 */
+	if(irq.major)
+    {
+        irq.devid = MKDEV(irq.major, 0);
+        register_chrdev_region(irq.devid, IRQ_CNT, IRQ_NAME);
+    }
+    else
+    {
+        alloc_chrdev_region(&irq.devid, 0, IRQ_CNT, IRQ_NAME);
+        irq.major = MAJOR(irq.devid);
+        irq.minor = MINOR(irq.devid);
+    }
 
+    /* 2、注册字符设备 */
+    cdev_init(&irq.cdev, &irq_fops);
+    cdev_add(&irq.cdev, irq.devid, IRQ_CNT);
+
+    /* 3、创建类 */
+    irq.class = class_create(THIS_MODULE, IRQ_NAME);
+    if(IS_ERR(irq.class))
+    {
+        return PTR_ERR(irq.class);
+    }
+
+    /* 4、创建设备 */
+    irq.device = device_create(irq.class, NULL, irq.devid, NULL, IRQ_NAME);
+    if(IS_ERR(irq.device))
+    {
+        return PTR_ERR(irq.device);
+    }
+
+    /* 5、初始化按键 */
+    atomic_set(&irq.keyvalue, INVAKEY);
+    atomic_set(&irq.releasekey, 0);
+
+    keyio_init();
+
+    return 0;
 }
 
 /**
@@ -219,8 +328,23 @@ static int __init irq_init(void)
  */
 static void __exit irq_exit(void)
 {
-
+    unsigned int i = 0;
     
+    /* 删除定时器 */
+	del_timer_sync(&irq.timer);
+
+    /* 释放中断 */
+	for(i = 0; i < KEY_NUM; i++)
+    {
+        free_irq(irq.irqkeydesc[i].irqnum, &irq);
+    }
+    cdev_del(&irq.cdev);
+
+    unregister_chrdev_region(irq.devid, IRQ_CNT);
+
+    device_destroy(irq.class, irq.devid);
+
+    class_destroy(  irq.class);
 }
 
 module_init(irq_init);
