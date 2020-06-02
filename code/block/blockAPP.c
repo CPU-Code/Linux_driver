2d4c0487a55bff8a44845069d7a041044165d81c
CPU_Code <923992029@qq.com>
2020-06-02 19:12:10 +0800

    堵塞IO

---
+++ code/block/blockAPP.c
@@ -0,0 +1,57 @@
+/*
+ * @Author: cpu_code
+ * @Date: 2020-06-02 13:19:02
+ * @LastEditTime: 2020-06-02 16:35:24
+ * @FilePath: \Linux_driver\code\block\blockAPP.c
+ * @Gitee: https://gitee.com/cpu_code
+ * @CSDN: https://blog.csdn.net/qq_44226094
+ */ 
+
+#include "stdio.h"
+#include "unistd.h"
+#include "sys/types.h"
+#include "sys/stat.h"
+#include "fcntl.h"
+#include "stdlib.h"
+#include "string.h"
+#include "linux/ioctl.h"
+
+int main(int argc, char *argv[])
+{
+    int fd;
+	int ret = 0;
+	char *filename;
+	unsigned char data;
+
+    if(argc != 2)
+    {
+        printf("Error Usage!\r\n");
+        return -1;
+    }
+
+    filename = argv[1];
+    fd = open(filename, O_RDWR);
+    if(fd < 0)
+    {
+        printf("Can't open file %s\r\n", filename);
+        return -1;
+    }
+    while (1)
+    {
+        ret = read(fd, &data, sizeof(data));
+        if(ret < 0)
+        {
+            /* 数据读取错误或者无效 */
+        }
+        else
+        {
+            /* 数据读取正确 */
+            if(data)	/* 读取到数据 */
+            {
+                printf("key value = %#X\r\n", data);
+            }
+        }
+        close(fd);
+    }
+    return ret;
+}
