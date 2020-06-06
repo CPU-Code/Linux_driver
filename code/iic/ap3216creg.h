85aa56f5ad3096616faa080f613a0cade274b09b
CPU_Code <923992029@qq.com>
2020-06-06 20:58:03 +0800

    iic：陀螺仪IIC协议

---
+++ code/iic/ap3216creg.h
@@ -0,0 +1,25 @@
+/*
+ * @Author: cpu_code
+ * @Date: 2020-06-05 22:49:14
+ * @LastEditTime: 2020-06-05 22:51:45
+ * @FilePath: \Linux_driver\code\iic\ap3216creg.h
+ * @Gitee: https://gitee.com/cpu_code
+ * @CSDN: https://blog.csdn.net/qq_44226094
+ */ 
+#ifndef AP3216C_H
+#define AP3216C_H
+
+#define AP3216C_ADDR    	0X1E	/* AP3216C器件地址  */
+
+/* AP3316C寄存器 */
+#define AP3216C_SYSTEMCONG	0x00	/* 配置寄存器       */
+#define AP3216C_INTSTATUS	0X01	/* 中断状态寄存器   */
+#define AP3216C_INTCLEAR	0X02	/* 中断清除寄存器   */
+#define AP3216C_IRDATALOW	0x0A	/* IR数据低字节     */
+#define AP3216C_IRDATAHIGH	0x0B	/* IR数据高字节     */
+#define AP3216C_ALSDATALOW	0x0C	/* ALS数据低字节    */
+#define AP3216C_ALSDATAHIGH	0X0D	/* ALS数据高字节    */
+#define AP3216C_PSDATALOW	0X0E	/* PS数据低字节     */
+#define AP3216C_PSDATAHIGH	0X0F	/* PS数据高字节     */
+
+#endif
